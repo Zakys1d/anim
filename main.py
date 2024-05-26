7c52251fb749ed99200b3a3262692c460756b98d
Zakys1d <147971792+Zakys1d@users.noreply.github.com>
2024-05-26 17:33:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,76 @@
+from typing import Any
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, position):
+        self.sheet = pygame.image.load('serge.png')
+        self.sheet.set_clip(pygame.Rect(0, 0,52,76))
+        self.image = self.sheet.subsurface(self.sheet.get_clip())
+        self.rect = self.image.get_rect()
+        self.rect.topleft = position
+        self.frame = 0
+        self.down = {0: (0,0,52,76), 1: (0,0,52,76), 2: (104,0,52,76), 3: (156,0,52,76)}
+        self.left = {0: (0,76,52,76), 1: (0,76,76), 2: (104,76,52,76), 3: (156,76,52,76)}
+        self.up = {0: (0,228,52,76), 1: (0,228,76), 2: (104,228,52,76), 3: (156,228,52,76)}
+        self.right = {0: (0,152,52,76), 1: (0,152,76), 2: (104,152,52,76), 3: (156,152,52,76)}
+       
+
+    def update_frame(self, frame_set):
+        self.frame += 1
+        if self.frame > len(frame_set) - 1:
+            self.frame = 0
+        return frame_set[self.frame]
+
+    def clip(self, clip_rect):
+        if type(clip_rect) is dict:
+            self.sheet.set_clip(pygame.Rect(self.update_frame(clip_rect)))
+        else:
+            self.sheet.set_clip(pygame.Rect(clip_rect))
+        return clip_rect
+    
+    def update(self, direction):
+        if direction == 'down':
+            self.clip(self.down)
+            self.rect.y += 5
+        elif direction == 'left':
+            self.clip(self.left)
+            self.rect.x -= 5
+        elif direction == 'up':
+            self.clip(self.up)
+            self.rect.y -= 5
+        elif direction == 'right':
+            self.clip(self.right)
+            self.rect.x += 5
+
+        if direction == "stand_down":
+            self.clip(self.down[0])
+        elif direction == "stand_left":
+            self.clip(self.left[0])
+        elif direction == "stand_up":
+            self.clip(self.up[0])
+        elif direction == "stand_right":
+            self.clip(self.right[0])
+
+    def events(self, event):
+        if event.type == pygame.QUIT:
+            game = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                self.update("down")
+            elif event.key == pygame.K_LEFT:
+                self.update("left")
+            elif event.key == pygame.K_UP:
+                self.update("up")
+            elif event.key == pygame.K_RIGHT:
+                self.update("right")
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                self.update("stand_down")
+            elif event.key == pygame.K_LEFT:
+                self.update("stand_left")
+            elif event.key == pygame.K_UP:
+                self.update("stand_up")
+            elif event.key == pygame.K_RIGHT:
+                self.update("stand_right")         
+
+    
